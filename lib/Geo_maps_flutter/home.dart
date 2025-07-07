ea46767f59e0879d1959a251724baf60ea2cead8
qeisandi <qeis.andi02@gmail.com>
2025-07-07 08:45:12 +0700

    updated

--- lib/Geo_maps_flutter/home.dart
+++ lib/Geo_maps_flutter/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GeoLocation extends StatefulWidget {
   const GeoLocation({super.key});
@@ -18,65 +18,101 @@ class _GeoLocationState extends State<GeoLocation> {
 
   Future<void> _getCurrentLocation() async {
     bool serviceEnable = await Geolocator.isLocationServiceEnabled();
-    if(!serviceEnable) {
+    if (!serviceEnable) {
       await Geolocator.openLocationSettings();
       return;
     }
 
     LocationPermission permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied || permission == LocationPermission.deniedForever) {
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
       permission = await Geolocator.requestPermission();
-      if (permission != LocationPermission.whileInUse && permission != LocationPermission.always) {
+      if (permission != LocationPermission.whileInUse &&
+          permission != LocationPermission.always) {
         return;
       }
     }
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
     _currentPosition = LatLng(position.latitude, position.longitude);
 
-    List<Placemark> placemark = await placemarkFromCoordinates(_currentPosition.latitude, _currentPosition.longitude);
+    List<Placemark> placemark = await placemarkFromCoordinates(
+      _currentPosition.latitude,
+      _currentPosition.longitude,
+    );
     Placemark place = placemark[0];
 
     setState(() {
-      _marker = Marker(markerId: MarkerId("Lokasi Saya"),
-      position: _currentPosition,
-      infoWindow: InfoWindow(title: "Lokasi Anda",snippet: "${place.street},${place.locality}",),);
+      _marker = Marker(
+        markerId: MarkerId("Lokasi Saya"),
+        position: _currentPosition,
+        infoWindow: InfoWindow(
+          title: "Lokasi Anda",
+          snippet: "${place.street},${place.locality}",
+        ),
+      );
 
-      _currentAdress = "${place.name},${place.street},${place.locality},${place.country}";
+      _currentAdress =
+          "${place.name},${place.street},${place.locality},${place.country}";
 
-      mapController?.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: _currentPosition,zoom: 16),));
+      mapController?.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(target: _currentPosition, zoom: 16),
+        ),
+      );
     });
   }
+
   @override
   void initState() {
     super.initState();
     _getCurrentLocation();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Google Map + Geolocator + Geocoding',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black),),),
+      appBar: AppBar(
+        title: Text(
+          'Google Map + Geolocator + Geocoding',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+      ),
       body: Stack(
         children: [
-          GoogleMap(initialCameraPosition: CameraPosition(target: _currentPosition,zoom: 14,
-          ),
-          onMapCreated: (controller) {
-            mapController = controller;
-          },
-          markers: _marker != null ? {_marker!} : {},
-          myLocationButtonEnabled: true,
-          myLocationEnabled: true,
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: _currentPosition,
+              zoom: 14,
+            ),
+            onMapCreated: (controller) {
+              mapController = controller;
+            },
+            markers: _marker != null ? {_marker!} : {},
+            myLocationButtonEnabled: true,
+            myLocationEnabled: true,
           ),
           Positioned(
             top: 16,
             left: 16,
             right: 16,
-            child: Card(color: Colors.white,
-            elevation: 4,
-            child: Padding(padding: EdgeInsetsGeometry.all(12.0),
-            child: Text(_currentAdress,style: TextStyle(fontSize: 14),),),))
+            child: Card(
+              color: Colors.white,
+              elevation: 4,
+              child: Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Text(_currentAdress, style: TextStyle(fontSize: 14)),
+              ),
+            ),
+          ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(onPressed: _getCurrentLocation,child: Icon(Icons.refresh),tooltip: "Refresh Lokasi",),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getCurrentLocation,
+        tooltip: "Refresh Lokasi",
+        child: Icon(Icons.refresh),
+      ),
     );
   }
 }
